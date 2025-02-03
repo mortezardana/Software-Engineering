cd4016552443d576ad9e8d872f741987d1882c17
Dexter <dexter.rowland@outlook.com>
2025-02-03 12:32:36 +0000

    Dexter's Commit

---
+++ pg-sd2-revised/pg-sd2/app/app.js
@@ -0,0 +1,93 @@
+// Import express.js
+const express = require("express");
+
+// Create express app
+var app = express();
+
+// Add static files location
+app.use(express.static("static"));
+
+// Get the functions in the db.js file to use
+const db = require('./services/db');
+
+// Create a route for root - /
+app.get("/", function(req, res) {
+    res.send("Hello Dexter!");
+});
+
+//Create a route for /roehampton
+
+app.get("/roehampton",function(req,res){
+    console.log(req.url)
+    let path = req.url;
+    res.send(path.substring(0,3));
+});
+
+app.get("/hello/:name", function(req,res){
+    console.log(req.params);
+    res.send("Hello " + req.params.name);
+});
+
+app.get("/user/:id", function(req,res){
+    console.log(req.params);
+    res.send(req.params.id);
+});
+
+app.get("/student/:name/:id", function(req,res){
+    console.log(req.params);
+    let name = req.params.name;
+    let id = req.params.id;
+    let output = "";
+
+    output += `<HTML>
+    <table id="myTable" border=1>
+        <tr>
+            <td>name</td>
+            <td>id</td>
+        </tr>
+    </table>
+    </HTML>`;
+    
+    res.send(output);
+});
+
+app.get("/db_test/:id", function(req,res){
+    console.log(req.params);
+    let sql = ("SELECT * from test_table WHERE id = " + req.params.id);
+    db.query(sql).then(results => {
+        console.log(results);
+        res.send(results)
+    });
+});
+
+// Create a route for testing the db
+app.get("/db_test", function(req, res) {
+    // Assumes a table called test_table exists in your database
+    sql = 'select * from test_table';
+    db.query(sql).then(results => {
+        console.log(results);
+        res.send(results)
+    });
+});
+
+// Create a route for /goodbye
+// Responds to a 'GET' request
+app.get("/goodbye", function(req, res) {
+    res.send("Goodbye world!");
+});
+
+// Create a dynamic route for /hello/<name>, where name is any value provided by user
+// At the end of the URL
+// Responds to a 'GET' request
+app.get("/hello/:name", function(req, res) {
+    // req.params contains any parameters in the request
+    // We can examine it in the console for debugging purposes
+    console.log(req.params);
+    //  Retrieve the 'name' parameter and use it in a dynamically generated page
+    res.send("Hello " + req.params.name);
+});
+
+// Start server on port 3000
+app.listen(3000,function(){
+    console.log(`Server running at http://127.0.0.1:3000/`);
+});
