c5e04f9b5dde741278b26236c9984c2fb2e9874c
MorteZA <mortezamrd75@gmail.com>
2025-04-06 23:07:21 +0100

    Reformat. Add new function.

--- pg-sd2-revised/pg-sd2/app/repository/activityRepo.js
+++ pg-sd2-revised/pg-sd2/app/repository/activityRepo.js
@@ -3,123 +3,132 @@ const Activity = require('../model/Activity');
 const ActivityType = require('../model/ActivityType');
 
 class ActivityRepository {
-  // Get all activities
-  static getAllActivities(filters = {}, page = 1, limit = 10) {
-    return new Promise((resolve, reject) => {
-      let whereClause = 'WHERE 1=1';
-      let queryParams = [];
+    // Get all activities
+    static getAllActivities(filters = {}, page = 1, limit = 10) {
+        return new Promise((resolve, reject) => {
+            let whereClause = 'WHERE 1=1';
+            let queryParams = [];
 
-      if (filters.type) {
-        whereClause += ' AND type = ?';
-        queryParams.push(filters.type);
-      }
+            if (filters.type) {
+                whereClause += ' AND type = ?';
+                queryParams.push(filters.type);
+            }
 
-      if (filters.member) {
-        whereClause += ' AND member = ?';
-        queryParams.push(filters.member);
-      }
+            if (filters.member) {
+                whereClause += ' AND member = ?';
+                queryParams.push(filters.member);
+            }
 
-      const offset = (page - 1) * limit;
-      const query = `SELECT * FROM activity ${whereClause} LIMIT ? OFFSET ?`;
+            const offset = (page - 1) * limit;
+            const query = `SELECT * FROM activity ${whereClause} LIMIT ? OFFSET ?`;
+            console.log("Executing query:", query, "with params:", queryParams);
 
-      queryParams.push(limit, offset);
+            queryParams.push(limit, offset);
 
-      connection.query(query, queryParams, (err, results) => {
-        if (err) {
-          reject(err);
-          return;
-        }
+            connection.query(query, queryParams, (err, results) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
 
-        const activities = results.map((activityData) => {
-          return new Activity(
-              activityData.id,
-              activityData.averageSpeed,
-              activityData.distance,
-              activityData.elevation,
-              activityData.member,
-              activityData.movingTime
-          );
+                const activities = results.map((activityData) => {
+                    return new Activity(activityData.id, activityData.type, activityData.averageSpeed, activityData.distance, activityData.elevation, activityData.member, activityData.movingTime);
+                });
+
+                resolve(activities);
+            });
         });
+    }
 
-        resolve(activities);
-      });
-    });
-  }
+    // Get an activity by ID
+    static getActivityById(id) {
+        return new Promise(async (resolve, reject) => {
+            try {
+                const query = 'SELECT * FROM activity WHERE id = ?';
+                console.log("Executing query:", query, "with params:", id);
+                const results = await connection.query(query, [id]);
 
-  // Get an activity by ID
-  static getActivityById(id) {
-    return new Promise(async (resolve, reject) => {
-      try {
-        const query = 'SELECT * FROM activity WHERE id = ?';
-        const results = await connection.query(query, [id]);
+                if (results.length > 0) {
+                    const activityData = results[0];
 
-        if (results.length > 0) {
-          const activityData = results[0];
+                    const validType = Object.values(ActivityType).includes(activityData.type) ? activityData.type : null;
 
-          const validType = Object.values(ActivityType).includes(activityData.type)
-              ? activityData.type
-              : null;
+                    const activity = new Activity(activityData.id, validType, activityData.averageSpeed, activityData.distance, activityData.elevation, activityData.member, // Assuming member is mapped separately
+                        activityData.movingTime);
+                    resolve(activity);
+                } else {
+                    resolve(null); // No activity found
+                }
+            } catch (error) {
+                reject(error);
+            }
+        });
+    }
 
-          const activity = new Activity(
-              activityData.id,
-              validType,
-              activityData.averageSpeed,
-              activityData.distance,
-              activityData.elevation,
-              activityData.member, // Assuming member is mapped separately
-              activityData.movingTime
-          );
-          resolve(activity);
-        } else {
-          resolve(null); // No activity found
-        }
-      } catch (error) {
-        reject(error);
-      }
-    });
-  }
+    // Add a new activity
+    static addActivity(activity) {
+        return new Promise((resolve, reject) => {
+            const query = 'INSERT INTO activity (averageSpeed, distance, elevation, member, movingTime) VALUES (?, ?, ?, ?, ?)';
+            console.log("Executing query:", query, "with params:", [activity.getAverageSpeed(), activity.getDistance(), activity.getElevation(), activity.getMember(), activity.getMovingTime()]);
+            connection.query(query, [activity.getAverageSpeed(), activity.getDistance(), activity.getElevation(), activity.getMember(), activity.getMovingTime()], (err, results) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(results.insertId); // Return the new activity's ID
+            });
+        });
+    }
 
-  // Add a new activity
-  static addActivity(activity) {
-    return new Promise((resolve, reject) => {
-      const query = 'INSERT INTO activity (averageSpeed, distance, elevation, member, movingTime) VALUES (?, ?, ?, ?, ?)';
-      connection.query(query, [activity.getAverageSpeed(), activity.getDistance(), activity.getElevation(), activity.getMember(), activity.getMovingTime()], (err, results) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(results.insertId); // Return the new activity's ID
-      });
-    });
-  }
+    // Update an activity
+    static updateActivity(id, activity) {
+        return new Promise((resolve, reject) => {
+            const query = 'UPDATE activity SET averageSpeed = ?, distance = ?, elevation = ?, movingTime = ? WHERE id = ?';
+            console.log("Executing query:", query, "with params:", [activity.getAverageSpeed(), activity.getDistance(), activity.getElevation(), activity.getMovingTime(), id]);
+            connection.query(query, [activity.getAverageSpeed(), activity.getDistance(), activity.getElevation(), activity.getMovingTime(), id], (err, results) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(results.affectedRows);
+            });
+        });
+    }
 
-  // Update an activity
-  static updateActivity(id, activity) {
-    return new Promise((resolve, reject) => {
-      const query = 'UPDATE activity SET averageSpeed = ?, distance = ?, elevation = ?, movingTime = ? WHERE id = ?';
-      connection.query(query, [activity.getAverageSpeed(), activity.getDistance(), activity.getElevation(), activity.getMovingTime(), id], (err, results) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(results.affectedRows);
-      });
-    });
-  }
+    // Delete an activity
+    static deleteActivity(id) {
+        return new Promise((resolve, reject) => {
+            const query = 'DELETE FROM activity WHERE id = ?';
+            console.log("Executing query:", query, "with params:", id);
+            connection.query(query, [id], (err, results) => {
+                if (err) {
+                    reject(err);
+                    return;
+                }
+                resolve(results.affectedRows);
+            });
+        });
+    }
 
-  // Delete an activity
-  static deleteActivity(id) {
-    return new Promise((resolve, reject) => {
-      const query = 'DELETE FROM activity WHERE id = ?';
-      connection.query(query, [id], (err, results) => {
-        if (err) {
-          reject(err);
-          return;
-        }
-        resolve(results.affectedRows);
-      });
-    });
-  }
+    static async getActivitiesByMemberId(memberId) {
+        return new Promise(async (resolve, reject) => {
+            const query = 'SELECT * FROM activity WHERE member_id = ?';
+            console.log("Executing query:", query, "with params:", memberId);
+            const results = await connection.query(query, [memberId]);
+            const activities = results.map((activityData) => {
+                return new Activity(
+                    activityData.id,
+                    activityData.type,
+                    activityData.averageSpeed,
+                    activityData.distance,
+                    activityData.elevation,
+                    activityData.member,
+                    activityData.movingTime
+                );
+            });
+            resolve(activities)
+        });
+    }
 }
 
 module.exports = ActivityRepository;
