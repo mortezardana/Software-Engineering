7f682e97feddd59300487f905ed903b5df391ff6
Mr Green <avinashsaluja311004@gmail.com>
2025-04-07 02:04:36 +0100

    db.js fix

--- pg-sd2-revised/pg-sd2/app/repository/member-repo.js
+++ pg-sd2-revised/pg-sd2/app/repository/member-repo.js
@@ -37,7 +37,7 @@ const getMembers = () => {
   });
 };
 
-// Function to get a member by ID
+// Function to get a member by their ID
 const getMemberById = (id) => {
   return new Promise((resolve, reject) => {
     const query = 'SELECT * FROM members WHERE id = ?';  // SQL query to get member by id
--- pg-sd2-revised/pg-sd2/app/services/db.js
+++ pg-sd2-revised/pg-sd2/app/services/db.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const mysql = require('mysql2/promise');
 
 const config = {
-  db: { /* do not put password or any sensitive info here, done only for demo */
+  db: { /* do not put password or any sensitive info here, done only for demoes */
     host: process.env.DB_CONTAINER,
     port: process.env.DB_PORT,
     user: process.env.MYSQL_ROOT_USER,
